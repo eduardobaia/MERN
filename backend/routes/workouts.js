b14009dbd4fab7040324a7b4828f6c73d50519ca
Eduardo Baia <eduardobaia@Eduardos-Air.Home>
2022-07-20 15:10:05 +0100

    add controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,34 +1,22 @@
-const e = require('express')
 const express = require('express')
-
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts
+} = require('../controllers/workoutController')
 const router = express.Router()
 const Workout = require('../models/workoutModel')
 
-router.get('/', (req,res) => {
-    res.json({msg: 'GET ALL WORKOUTS'})
-})
+//get all workouts
+router.get('/', getWorkouts)
 
 
 //get a single workout
-router.get('/:id', (req,res) => {
-    res.json({msg: 'GET a single '})
-})
+router.get('/:id', getWorkout )
 
 
 // POST a new workout u
-router.post('/', async (req,res) =>{
-    const {title, load, reps} = req.body
-
-
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message}) 
-    }
-
-    res.json({msg: "POST a new workout "})
-})
+router.post('/', createWorkout)
 
 // DELETE a new workout u
 router.delete('/:id', (req,res) =>{
@@ -44,4 +32,4 @@ router.patch('/:id', (req,res) =>{
 
 
 
-module.exports= router
+module.exports= router 
